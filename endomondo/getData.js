28c3e13c45e486a1c67710b7ac6474a515949d9b
Micro <maglars2@gmail.com>
2015-11-30 09:23:38 +0100

    Update getData.js
    Updated info based on new github repo

--- endomondo/getData.js
+++ endomondo/getData.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Endomondo data retriever
-// @namespace https://github.com/Miicroo/endomondo/blob/master/
+// @namespace https://github.com/Miicroo/TamperMonkey/tree/master/endomondo
 // @version 1.0
 // @description Retrieves workout data from endomondo.
 // @author Miicroo
@@ -8,16 +8,10 @@
 // @grant none
 // @copyright 2015+, Micro
 // @require http://code.jquery.com/jquery-latest.js
+// @require https://raw.githubusercontent.com/Miicroo/TamperMonkey/master/common/common.js
 // ==/UserScript==
 
 $(document).ready(function() {
-	Date.prototype.yyyymmdd = function() {
-        var yyyy = this.getFullYear().toString();
-        var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
-        var dd = this.getDate().toString();
-        return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]); // padding
-    };
-	
     if(isLoggedIn()) {
         addInput();
     }
@@ -28,13 +22,13 @@ function isLoggedIn() {
 }
 
 function addInput() {
-    var container = $('body')[0]; //$('ul', $('.header-right')[0].innerHTML)[0];
+    var container = $('body')[0];
 	
     var startDate = '<input type="text" value="2015-01-01" id="startDate" />';
     var endDate = '<input type="text" value="'+((new Date()).yyyymmdd())+'" id="endDate" />';
-	var button = '<button id="startDownload">Download</button>'; // Button to trigger download
+    var button = '<button id="startDownload">Download</button>'; // Button to trigger download
     
-	// Add elements to container
+    // Add elements to container
     container.innerHTML = startDate+' '+endDate+' '+button+' '+container.innerHTML;
     
     var dlButton = document.getElementById('startDownload');
@@ -44,8 +38,8 @@ function addInput() {
 function retrieveData() {
     var current = new Date(document.getElementById('startDate').value);
     var last = new Date(document.getElementById('endDate').value);
-	var dayAsMs = 86400000;
-	last = new Date(last.getTime()+dayAsMs); // Add one day to ensure that the last day is included
+    var dayAsMs = 86400000;
+    last = new Date(last.getTime()+dayAsMs); // Add one day to ensure that the last day is included
 	
     var profileId = getProfileId();
     
@@ -55,7 +49,7 @@ function retrieveData() {
 function getProfileId() {
     var profileId = '';
     var profilePattern = 'profile/';
-	var links = $('a', $('.personalMenu-menu')[0].innerHTML);
+    var links = $('a', $('.personalMenu-menu')[0].innerHTML);
     for(i = 0; i<links.length; i++) {
         var index = links[i].href.indexOf(profilePattern);
         if(index != -1) {
@@ -71,15 +65,6 @@ function getData(profileId, startDate, endDate) {
     $.get('../rest/v1/users/'+profileId+'/workouts?before='+endDate+'&after='+startDate, function(data) {
         // Data arrives as JavaScript object!
         data = '{"data":'+JSON.stringify(data)+'}';
-        download(data); // Download data as JSON
+        download(data, 'endomondoData.json'); // Download data as JSON
     });
-}
-
-function download(data) {
-    var hiddenLink = document.createElement('a');
-
-    hiddenLink.href = 'data:attachment/text,' + encodeURI(data);
-    hiddenLink.target = '_blank';
-    hiddenLink.download = 'endomondoData.json';
-    hiddenLink.click();
 }
