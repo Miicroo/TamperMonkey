4fb99f87c1669c2cf47c7c19e43bfa5ecb987417
Magnus Larsson <maglars2@gmail.com>
2020-07-05 21:41:32 +0200

    Create order_lowest_price.js

---
+++ matse/order_lowest_price.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Sort mat.se price low-high
+// @namespace    https://github.com/Miicroo/TamperMonkey/tree/master/matse
+// @version      1.0.0
+// @description  Mat.se: Sort price low-high
+// @author       Miicroo
+// @match        https://www.mat.se/search.html*
+// @grant        none
+// ==/UserScript==
+
+const hasSearchQuery = window.location.href.indexOf('?') !== -1;
+const hasSortOrder = window.location.href.indexOf('sortOrder') !== -1;
+
+if (hasSearchQuery && !hasSortOrder) {
+    window.location.href += `&sortOrder=Pris%20lågt-högt`;
+}
