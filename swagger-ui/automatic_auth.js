92b6405a024f9babf2fc1c0716a7dac11f7ae527
Magnus Larsson <maglars2@gmail.com>
2023-10-24 10:12:08 +0200

    Update automatic_auth.js to v1.1

--- swagger-ui/automatic_auth.js
+++ swagger-ui/automatic_auth.js
@@ -1,20 +1,24 @@
 // ==UserScript==
 // @name         Automatic swagger auth
 // @namespace    https://github.com/Miicroo/TamperMonkey/tree/master/swagger-ui
-// @version      1.0
+// @version      1.1
 // @description  Automatic swagger auth
 // @author       Miicroo
-// @match        <your_swagger_ui_url>
+// @match        */api_docs*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=swagger.io
 // @grant        none
 // @copyright    2022+, Miicroo
 // ==/UserScript==
 
 
-// Value of the auth input, e.g. an api_key
-const AUTH_VALUE = '';
+// AUTHS contains objects with index and value:
 // Index of the auth form in the GUI, e.g. 0 if the form is on top of the auth dialogue box, 1 if it is in second place...
-const AUTH_INDEX = -1;
+// Value of the auth input, e.g. an api_key
+
+const AUTHS = [
+    {'index': 0, 'value': '<some_value>'},
+    {'index': 1, 'value': '<some_other_value>'}
+];
 
 const inputTypesWithValueSetter = [
     window.HTMLInputElement,
@@ -54,15 +58,20 @@ function authorize() {
     authDialogButton.click();
 
     const authContainers = document.querySelectorAll('.auth-container');
-    const authContainer = authContainers[AUTH_INDEX];
-    const buttons = Array.from(authContainer.querySelectorAll('button'));
+    let closeButton = undefined;
+    for (let i = 0; i<AUTHS.length; i++) {
+        const index = AUTHS[i].index;
+        const authContainer = authContainers[index];
+        const buttons = Array.from(authContainer.querySelectorAll('button'));
 
-    const authInput = authContainer.querySelector('input[type="text"]');
-    const authButton = buttons.filter(button => button.innerText === 'Authorize')[0]; // There can be only one...
-    const closeButton = buttons.filter(button => button.innerText === 'Close')[0];
+        const authInput = authContainer.querySelector('input[type="text"]');
+        const authButton = buttons.filter(button => button.innerText === 'Authorize')[0]; // There can be only one...
+        closeButton = buttons.filter(button => button.innerText === 'Close')[0];
 
-    reactTriggerInputChange(authInput, AUTH_VALUE);
+        reactTriggerInputChange(authInput, AUTHS[i].value);
 
-    authButton.click();
+        authButton.click();
+    }
+
     closeButton.click();
 }
